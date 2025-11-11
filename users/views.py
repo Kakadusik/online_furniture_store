d116d95737e4ca86ec52dee362e42ae36e1abef9
Kakadusik <hitrinka@yahoo.com>
2025-11-11 14:23:23 +0300

    Templates for users app added

---
+++ users/views.py
@@ -0,0 +1,23 @@
+from urllib import response
+from django.shortcuts import render
+
+def login(request):
+    context = {
+        'title': 'Home - Логин'
+    }
+    return render(request, 'users/login.html', context)
+
+def registration(request):
+    context = {
+        'title': 'Home - Регистрация'
+    }
+    return render(request, 'users/registration.html', context)
+
+def profile(request):
+    context = {
+        'title': 'Home - Кабинет'
+    }
+    return render(request, 'users/profile.html', context)
+
+def logout(request):
+    pass
